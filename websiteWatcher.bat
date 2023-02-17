b0864dba35946ff9af49302a6693ff2694822f7a
Joseph Ermatinger <54058020+JosephErmatinger@users.noreply.github.com>
2023-02-17 01:53:41 -0500

    Add files via upload

--- websiteWatcher.bat
+++ websiteWatcher.bat
@@ -1,10 +1,15 @@
 @echo off
 
 :start
+title Website Watcher
 echo Website change tracker developed by Joe Ermatinger
 echo Today is %Date% %Time%
 
+:display
+echo View List of URLs to be processed!
 type url.txt
+notepad url.txt
+pause
 
 :prompt
 set /p url="Enter url "
@@ -17,5 +22,6 @@ for /f %%F in (url.txt) do (
 	echo Downloading new %%F
 	curl %%F >> new.txt
 )
+:finish
 echo Program Finished!
 echo It is %Date% %Time%
